2ca97d2f01645bf67345d62486d7a4fd1b35676d
Anna <ann_dmitirevna97@mail.ru>
2024-09-13 13:56:45 +0300

    финальный вариант

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,7 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
-      keyframes: {
-        slide: {
-          '0%': {
-            transform: 'translateY(-130px)',
-          },
-
-          '25%': {
-            transform: 'translateY(calc(160px - 100%))',
-          },
-
-          '100%': {
-            transform: 'translateY(calc(170px - 100%))',
-          },
-        },
-      },
-      animation: {
-        slide: 'slide 1s forwards',
-      },
+     
     },
   },
   plugins: [],
